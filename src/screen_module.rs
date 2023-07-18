fb5e0571e9d48544090631472068044e447e22c1
Willi Stepp <willi.stepp@epfl.ch>
2023-07-18 15:30:57 +0200

    updated dependencies

--- src/screen_module.rs
+++ src/screen_module.rs
@@ -5,17 +5,17 @@ use ndarray::parallel::prelude::*;
 pub fn screen<'a>(color1: ArrayView1<u8>, color2: ArrayView1<u8>,color_out:&'a mut Array1<u8>)
  -> &'a Array1<u8>{
 
-    let r = 255.0 - 255.0 * (1.0 - f32::from(color1[0])/255.0)
+    let r: f32 = 255.0 - 255.0 * (1.0 - f32::from(color1[0])/255.0)
      * (1.0 - f32::from(color2[0])/255.0);
-    let r_out = r.round() as u8;
-    let g = 255.0 - 255.0 * (1.0 - f32::from(color1[1])/255.0)
+    let r_out: u8 = r.round() as u8;
+    let g: f32 = 255.0 - 255.0 * (1.0 - f32::from(color1[1])/255.0)
      * (1.0 - f32::from(color2[1])/255.0);
-    let g_out = g.round() as u8;
-    let b = 255.0 - 255.0 * (1.0 - f32::from(color1[2])/255.0)
+    let g_out: u8 = g.round() as u8;
+    let b: f32 = 255.0 - 255.0 * (1.0 - f32::from(color1[2])/255.0)
      * (1.0 - f32::from(color2[2])/255.0);
-    let b_out = b.round() as u8;
-    let alpha = 255 - ((255 - color1[3]) * (255 - color2[3]) / 255);
-    let alpha_out = alpha as u8;
+    let b_out: u8 = b.round() as u8;
+    let alpha: u8 = 255 - ((255 - color1[3]) * (255 - color2[3]) / 255);
+    let alpha_out: u8 = alpha as u8;
 
     color_out[[0]] = r_out;
     color_out[[1]] = g_out;
@@ -26,7 +26,7 @@ pub fn screen<'a>(color1: ArrayView1<u8>, color2: ArrayView1<u8>,color_out:&'a m
 
 pub fn screen_frame(channel1: ArrayView3<u8>, channel2:ArrayView3<u8>) -> Array3<u8>{
 
-    let mut frame = Array3::<u8>::zeros(channel1.dim());
+    let mut frame: ndarray::ArrayBase<ndarray::OwnedRepr<u8>, ndarray::Dim<[usize; 3]>> = Array3::<u8>::zeros(channel1.dim());
 
     // Cycle through all the pixels to get the single color values to be passed to screen()
     for (row_index, mut row)
@@ -55,8 +55,9 @@ pub fn screen_stack(stack1: ArrayView4<u8>, stack2: ArrayView4<u8>) -> Array4<u8
     }).collect();
 
     // convert this Vec<Array3<u8>> to an Array4<u8>
-    let inner_shape = out[0].dim();
-    let shape = (out.len(), inner_shape.0, inner_shape.1, inner_shape.2);
+    let inner_shape: (usize, usize, usize) = out[0].dim();
+    println!("inner_shape");
+    let shape: (usize, usize, usize, usize) = (out.len(), inner_shape.0, inner_shape.1, inner_shape.2);
     let flat: Vec<u8> = out.iter().flatten().cloned().collect();
     Array4::from_shape_vec(shape, flat).unwrap()
 }
