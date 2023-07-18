ed9f7c66adfac036a95981e2f31f9983646a3c48
Willi Stepp <willi.stepp@epfl.ch>
2023-07-18 17:01:56 +0200

    Fixing pyarray for new versions

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,30 @@
-use pyo3::prelude::{pymodule, PyModule, PyResult, Python};
+extern crate pyo3;
+
 use numpy::{PyArray4, PyReadonlyArray4, IntoPyArray};
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use pyo3::Python;
 use ndarray::{ArrayView4, Array4};
+
 mod screen_module;
+
+fn screen_stack_wrap(stack1: ArrayView4<'_, u8>, stack2: ArrayView4<'_, u8>) -> Array4<u8>{
+    screen_module::screen_stack(stack1, stack2)
+}
+
+#[pyfunction]
+pub fn screen_stack_py<'py>(_py:Python<'py>, stack1: PyReadonlyArray4<'_, u8>,
+ stack2: PyReadonlyArray4<'_, u8>) -> &'py PyArray4<u8>{
+    let stack_1 = stack1.as_array();
+    let stack_2 = stack2.as_array();
+    let array: Array4<u8> = screen_stack_wrap(stack_1, stack_2);
+    array.into_pyarray(_py)
+}
+
 
 
 #[pymodule]
 fn img_rust(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-
-    fn screen_stack_wrap(stack1: ArrayView4<'_, u8>, stack2: ArrayView4<'_, u8>) -> Array4<u8>{
-        screen_module::screen_stack(stack1, stack2)
-    }
-
-    #[pyfn(m, "screen_stack_wrap")]
-    pub fn screen_stack_py<'py>(py: Python<'py>, stack1: PyReadonlyArray4<'_, u8>,
-     stack2: PyReadonlyArray4<'_, u8>) -> &'py PyArray4<u8>{
-        let stack_1 = stack1.as_array();
-        let stack_2 = stack2.as_array();
-        screen_stack_wrap(stack_1, stack_2).into_pyarray(py)
-    }
-
+    m.add_function(wrap_pyfunction!(screen_stack_py, m)?)?;
     Ok(())
 }
